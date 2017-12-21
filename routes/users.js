daeab3a9af9299e9bf3efb33021efd0dae41424a
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-21 18:18:44 +0000

    Added a view for users and data helpers to interact with database

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,18 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (knex) => {
+module.exports = (DataHelpers) => {
 
-  router.get("/", (req, res) => {
+  router.get("/games/:id", (req, res) => {
+    DataHelpers.getGameState(1).then((gameState) => {
+      let templateVars = { state: gameState };
+      console.log(gameState);
+      res.render("users", templateVars);
+    });
+  });
+
+// To visit user page
+  router.get("/users/:id", (req, res) => {
     knex
       .select("*")
       .from("users")
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -35,8 +36,12 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
+// Use data helpers to contact db
+const DataHelpers = require("./lib/util/data_helpers")(knex);
+
+
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use("/cards", usersRoutes(DataHelpers));
 
 // Home page
 app.get("/", (req, res) => {
