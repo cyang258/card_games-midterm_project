08b8ec35b1f26827e2038b4c9519aff84989ab9a
Chun Yang <canadachunyang@gmail.com>
2017-12-22 23:47:48 +0000

    socket connection setted and room setted

--- server.js
+++ server.js
@@ -56,20 +56,48 @@ const io          = require("socket.io").listen(server);
 
 var users = [];
 var connections = [];
+var rooms = [];
 
 
 
 io.on('connection', function(socket) {
 
-  connections.push(socket);
-  console.log(io)
+  //create room
+  socket.on("createRoom", function(room){
 
+      socket.join(room);
+      io.of('/').in(room).clients((error, clients) => {
+         if (error) throw error;
+         console.log(clients);
+      });
+
+  })
+  console.log(io.sockets.adapter.rooms);
+
+  //join a room
+  socket.on("join_A_Room", function(roomNumber){
+      socket.join(roomNumber);
+      console.log(io.sockets.adapter.rooms[roomNumber].length)
+      io.of('/').in(roomNumber).clients((error, clients) => {
+      if (error) throw error;
+        console.log(clients);
+      });
+  })
+
+
+  //How many people connected
+  connections.push(socket);
   console.log('Connected: %s sockets connected', connections.length)
 
   // Disconnect
   socket.on('disconnect', function(data){
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length)
+  })
+
+  socket.on('send message', function(data){
+    console.log(data);
+    io.emit('new message', data);
   })
 })
 
