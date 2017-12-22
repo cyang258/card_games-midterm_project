594da1465c4697b970e2fd1e0d073cbb58237a73
Chun Yang <canadachunyang@gmail.com>
2017-12-22 18:14:28 +0000

    changes for socket.io

--- server.js
+++ server.js
@@ -56,14 +56,14 @@ var connections = [];
 
 io.on('connection', function(socket) {
 
-
+  connections.push(socket);
   console.log(io)
 
   console.log('Connected: %s sockets connected', connections.length)
 
   // Disconnect
   socket.on('disconnect', function(data){
-
+    connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length)
   })
 })
